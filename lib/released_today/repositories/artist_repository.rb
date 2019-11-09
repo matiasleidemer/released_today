d0070c5facf652ea28ccccb6d5c0786254e67c87
Matias Leidemer <matiasleidemer@gmail.com>
2019-11-09 20:36:07 -0300

    Add ArtistRepository#stale method

--- lib/released_today/repositories/artist_repository.rb
+++ lib/released_today/repositories/artist_repository.rb
@@ -1,2 +1,9 @@
 class ArtistRepository < Hanami::Repository
+  TWENTY_TWO_HOURS = 22 * 60 * 60
+
+  def stale
+    artists
+      .where(disabled_at: nil)
+      .where { updated_at < Time.now - TWENTY_TWO_HOURS }
+  end
 end
--- spec/released_today/repositories/artist_repository_spec.rb
+++ spec/released_today/repositories/artist_repository_spec.rb
@@ -1,3 +1,24 @@
 RSpec.describe ArtistRepository, type: :repository do
-  # place your tests here
+  let(:repository) { ArtistRepository.new }
+
+  describe '#stale' do
+    let(:stale_time) { Time.now - ArtistRepository::TWENTY_TWO_HOURS }
+    let(:descendents) { Artist.new(name: 'Descendents', spotify_id: '1', updated_at: stale_time) }
+    let(:jawbreaker) { Artist.new(name: 'Jawbreaker', spotify_id: '2', updated_at: stale_time, disabled_at: Time.now) }
+    let(:black_flag) { Artist.new(name: 'Black Flag', spotify_id: '3') }
+    let(:hwm) { Artist.new(name: 'HWM', spotify_id: '4', disabled_at: Time.now) }
+
+    before do
+      repository.clear
+      repository.create(descendents)
+      repository.create(jawbreaker)
+      repository.create(black_flag)
+      repository.create(hwm)
+    end
+
+    it 'returns stale not disabled artists' do
+      stale_artists = repository.stale.call
+      expect(stale_artists.map(&:name)).to eql(['Descendents'])
+    end
+  end
 end
